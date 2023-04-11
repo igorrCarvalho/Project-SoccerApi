a4c7a61505a9a3d3e5c6c2762bfcab3db7efd363
Igor Carvalho <igorsilvabhz6@gmail.com>
2023-04-11 16:07:22 -0300

    Corrige nome errado nos seeders

--- app/backend/src/database/migrations/20230411174826-create-team.js
+++ app/backend/src/database/migrations/20230411174826-create-team.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Teams', {
+    await queryInterface.createTable('teams', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -14,6 +14,6 @@ module.exports = {
     });
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Teams');
+    await queryInterface.dropTable('teams');
   }
 };
